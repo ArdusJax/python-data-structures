ee7ff436d34e7330e668f55c9b56d351abeb2f71
Chris O'Brian <mobiletread@gmail.com>
2023-07-24 11:44:17 -0700

    compare for binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -93,3 +93,16 @@ def bfs(head: Optional[BinaryNode], needle):
             q.append(curr.right)
 
         return False
+
+
+def compare(a: Optional[BinaryNode], b: Optional[BinaryNode]):
+    if a is None and b is None:
+        return True
+
+    if a is None or b is None:
+        return False
+
+    if a.value != b.value:
+        return False
+
+    return compare(a.left, b.left) and compare(a.right, b.right)
