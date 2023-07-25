89ff84de82905a9ea37e37cdf0111c1a2a947b37
Chris O'Brian <mobiletread@gmail.com>
2023-07-25 12:31:39 -0700

    adding some other counting practice

--- list_and_dict_comp.py
+++ list_and_dict_comp.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+import re
+from typing import DefaultDict
+
 arr = [1, 2, 13, 44, 5, 33, 1, 21, 5, 19, 77, 9, 393, 1, 2, 99]
 
 
@@ -30,3 +34,28 @@ def find(key: int, d):
 find(2, d)
 find(3, d)
 find(4, d)
+
+
+def count_words(string):
+    counts = DefaultDict(int)
+    for word in re.findall("\w+", string):
+        counts[word] += 1
+    print(counts)
+
+
+s = "This is a sentence that has a lot of words in it. Please count all the words"
+count_words(s)
+
+
+def add_or_insert(string):
+    words = string.split(" ")
+    count = DefaultDict(int)
+    for item in words:
+        if item in count.keys():
+            count[item] += 1
+        else:
+            count[item] = 1
+    print(f"Add or insert: {count}")
+
+
+add_or_insert(s)
